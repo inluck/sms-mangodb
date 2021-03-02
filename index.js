f8d1d3e493c6eefdb60da034cbce004a696825a9
Jon Mitchell <jon@altinc.ca>
2021-03-02 20:12:51 -0330

    First Commit

--- index.js
+++ index.js
@@ -16,21 +16,34 @@ module.exports = require("./main.js")
 const http = require('http')
 const url = require('url');
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://96.126.70.135:27017/";
+var db_name = "sms"
+
 const app = http.createServer((request, response) => {
     const queryObject = url.parse(request.url,true).query;
     from = queryObject['from']
     phonenumber = queryObject['phonenumber']
     message = queryObject['message']
+    var re = new RegExp('^(\\d\\d\\d\\d\\d\\d)$');
+    var otp = re.exec(message);
     try {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db(db_name);
+            var myobj = { from: from, phonenumber: phonenumber, message: message, otp: otp};
+            dbo.collection("messages").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+            });
+        });
+
         console.log(from)
         console.log(phonenumber)
         console.log(message)
-
-        var re = new RegExp('^(\\d\\d\\d\\d\\d\\d)$');
-        var opt = re.exec(message);
-
+        
         response.writeHead(200, { 'Content-Type': 'application/json' })
-        // response.end(JSON.stringify(callflow_obj))
         response.end("{status: 'ok'}")
 
     }
